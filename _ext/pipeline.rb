e7214364bddb02229f73fcdf94cb90c784c22a88
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2013-03-14 11:11:43 +0100

    Generate site a lot faster + Upgrade to twitter bootstrap 2.3.1 + Drop JBoss.org stuff for now (not compatible with 2.3.1 yet)

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,18 +1,5 @@
-require 'wget_wrapper'
-require 'js_minifier'
-require 'css_minifier'
-require 'html_minifier'
-require 'file_merger'
-require 'compass_config'
 
 Awestruct::Extensions::Pipeline.new do
 	helper Awestruct::Extensions::Partial
 	helper Awestruct::Extensions::Relative
-
-	extension Awestruct::Extensions::WgetWrapper.new
-	transformer Awestruct::Extensions::JsMinifier.new
-	transformer Awestruct::Extensions::CssMinifier.new
-	transformer Awestruct::Extensions::HtmlMinifier.new
-	extension Awestruct::Extensions::FileMerger.new
-	extension Awestruct::Extensions::CompassConfig.new
 end
