5af819fd12e3d1a06a74fb3903adcdc3f8e1a82f
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2014-02-21 18:09:45 +0100

    asciidoc blog: prototype

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -2,4 +2,10 @@
 Awestruct::Extensions::Pipeline.new do
 	helper Awestruct::Extensions::Partial
 	helper Awestruct::Extensions::Relative
+
+    extension Awestruct::Extensions::Posts.new('/blog')
+    # extension Awestruct::Extensions::AutoTag.new(:posts)
+    extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 8)
+    extension Awestruct::Extensions::Tagger.new(:posts, '/blog/index', '/blog/tags', :per_page => 8)
+    extension Awestruct::Extensions::Disqus.new()
 end
