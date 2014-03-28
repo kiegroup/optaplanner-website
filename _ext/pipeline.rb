3ed2339b8a590c4294592632557d12be57f6bf56
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2014-03-28 11:25:50 +0100

    Atom: fix images in the atom feed

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -10,5 +10,5 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 5)
   extension Awestruct::Extensions::Tagger.new(:posts, '/blog/index', '/blog/tags', :per_page => 5)
   extension Awestruct::Extensions::Disqus.new
-  extension Awestruct::Extensions::Atomizer.new(:posts, '/blog/news.atom')
+  extension Awestruct::Extensions::Atomizer.new(:posts, '/blog/news.atom', :template => File.join( File.dirname(__FILE__), 'relative_template.atom.haml'))
 end
