67748d089b22f3800c0131f053efec16fc3fd520
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2015-01-19 15:49:02 +0100

    blog page should introduce people to each article, instead of showing all articles verbosily

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -7,8 +7,8 @@ Awestruct::Extensions::Pipeline.new do
 	helper Awestruct::Extensions::Optaplanner
 
   extension Awestruct::Extensions::Posts.new('/blog')
-  extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 5)
-  extension Awestruct::Extensions::Tagger.new(:posts, '/blog/index', '/blog/tags', :per_page => 5)
+  extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 10)
+  extension Awestruct::Extensions::Tagger.new(:posts, '/blog/index', '/blog/tags', :per_page => 10)
   extension Awestruct::Extensions::Disqus.new
   extension Awestruct::Extensions::Atomizer.new(:posts, '/blog/news.atom', :template => File.join( File.dirname(__FILE__), 'relative_template.atom.haml'))
 end
