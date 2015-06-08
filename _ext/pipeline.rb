c6fa8afde6d758ac8b2b5d0490f4bd1360f2c40f
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2015-06-08 16:47:33 +0200

    Add sitemap.xml

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -11,4 +11,5 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Tagger.new(:posts, '/blog/index', '/blog/tags', :per_page => 10)
   extension Awestruct::Extensions::Disqus.new
   extension Awestruct::Extensions::Atomizer.new(:posts, '/blog/news.atom', :template => File.join( File.dirname(__FILE__), 'relative_template.atom.haml'))
+  extension Awestruct::Extensions::Sitemap.new()
 end
