edf711c95417424005bfcb4febbc8f457dc86e24
Ondrej Skopek <skopekondrej@gmail.com>
2015-09-29 14:58:23 +0200

    PLANNER-431: Added hiding

--- website/optaplannerWebsite.js
+++ website/optaplannerWebsite.js
@@ -1,3 +1,23 @@
+function toggleShowHiddenSection(hiddenSection) {
+    hiddenSection.prev().toggleClass('stacked');
+    hiddenSection.toggle();
+}
+
+function insertShowSolutionButtons() {
+    $('.hidden-section').each(function(idx, node) {
+        var hiddenSection = $(node);
+        var showHiddenSectionButton = $('<a class="show-hidden-section" href="#">Show/hide</a>');
+        showHiddenSectionButton.insertBefore(hiddenSection);
+        showHiddenSectionButton.on('click', function(event) {
+            event.preventDefault();
+            toggleShowHiddenSection(hiddenSection);
+        });
+        toggleShowHiddenSection(hiddenSection);
+    });
+}
+
+$(insertShowSolutionButtons);
+
 $(document).ready( function() {
     $('.carousel').carousel();
     $('#whatIsCarousel').on('slide.bs.carousel', function(e) {
