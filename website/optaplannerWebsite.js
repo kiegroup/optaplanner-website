86afb4c73344dbbd1ca045aa897ac47c2ad897c1
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2015-10-14 17:42:46 +0200

    upgrade recipe priorities: Switch from icon to badges + minimize the verbosity of the adoc

--- website/optaplannerWebsite.js
+++ website/optaplannerWebsite.js
@@ -1,3 +1,31 @@
+function insertUpgradeRecipePriorities() {
+    $('.upgrade-recipe-major').each(function(idx, node) {
+        var sectionTitle = $(node.firstChild);
+        var badge = $('<span class="label label-danger label-as-badge">Major</span>');
+        $(node).prepend(badge);
+    });
+    $('.upgrade-recipe-minor').each(function(idx, node) {
+        var sectionTitle = $(node.firstChild);
+        var badge = $('<span class="label label-warning label-as-badge">Minor</span>');
+        $(node).prepend(badge);
+    });
+    $('.upgrade-recipe-impl-detail').each(function(idx, node) {
+        var sectionTitle = $(node.firstChild);
+        var badge = $('<span class="label label-info label-as-badge">Impl detail</span>');
+        $(node).prepend(badge);
+    });
+    $('.upgrade-recipe-recommended').each(function(idx, node) {
+        var sectionTitle = $(node.firstChild);
+        var badge = $('<span class="label label-primary label-as-badge">Recommended</span>');
+        $(node).prepend(badge);
+    });
+    $('.upgrade-recipe-readme').each(function(idx, node) {
+        var sectionTitle = $(node.firstChild);
+        var badge = $('<span class="label label-success label-as-badge">Readme</span>');
+        $(node).prepend(badge);
+    });
+}
+
 function toggleShowHiddenSection(hiddenSection) {
     hiddenSection.prev().toggleClass('stacked');
     hiddenSection.toggle();
@@ -16,6 +44,7 @@ function insertShowHiddenSectionButtons() {
     });
 }
 
+$(insertUpgradeRecipePriorities);
 $(insertShowHiddenSectionButtons);
 
 $(document).ready( function() {
