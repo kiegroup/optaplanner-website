7bde4316d75db3be264618488ef833062d77ba8f
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2015-10-15 15:52:48 +0200

    upgrade recipe priorities: Fix badges + minimize the verbosity of the adoc + put summary title for each recipe snippet

--- website/optaplannerWebsite.js
+++ website/optaplannerWebsite.js
@@ -1,26 +1,23 @@
 function insertUpgradeRecipePriorities() {
     $('.upgrade-recipe-major').each(function(idx, node) {
-        var sectionTitle = $(node.firstChild);
         var badge = $('<span class="label label-danger label-as-badge">Major</span>');
         $(node).prepend(badge);
     });
     $('.upgrade-recipe-minor').each(function(idx, node) {
-        var sectionTitle = $(node.firstChild);
+        $(node).children().not('h3:first').wrapAll('<div class="hidden-section"></div>');
         var badge = $('<span class="label label-warning label-as-badge">Minor</span>');
         $(node).prepend(badge);
     });
     $('.upgrade-recipe-impl-detail').each(function(idx, node) {
-        var sectionTitle = $(node.firstChild);
+        $(node).children().not('h3:first').wrapAll('<div class="hidden-section"></div>');
         var badge = $('<span class="label label-info label-as-badge">Impl detail</span>');
         $(node).prepend(badge);
     });
     $('.upgrade-recipe-recommended').each(function(idx, node) {
-        var sectionTitle = $(node.firstChild);
         var badge = $('<span class="label label-primary label-as-badge">Recommended</span>');
         $(node).prepend(badge);
     });
     $('.upgrade-recipe-readme').each(function(idx, node) {
-        var sectionTitle = $(node.firstChild);
         var badge = $('<span class="label label-success label-as-badge">Readme</span>');
         $(node).prepend(badge);
     });
@@ -44,10 +41,10 @@ function insertShowHiddenSectionButtons() {
     });
 }
 
-$(insertUpgradeRecipePriorities);
-$(insertShowHiddenSectionButtons);
-
 $(document).ready( function() {
+    $(insertUpgradeRecipePriorities);
+    $(insertShowHiddenSectionButtons);
+
     $('.carousel').carousel();
     $('#whatIsCarousel').on('slide.bs.carousel', function(e) {
         var next = $(e.relatedTarget);
