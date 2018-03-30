adfd3ab9fe840ea9cf68ee778d301f2af3b92d04
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2018-03-30 13:49:32 +0200

    Upgrade from awestruct 0.5.7 to awestruct 0.6.0.alpha1 to avoid verbose log messages on Fedora 27

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@ source 'https://rubygems.org'
 # ruby '2.3.1p112'
 
 gem 'rake', '>= 10.4.2'
-gem 'awestruct', '0.5.7'
-gem 'asciidoctor', '1.5.5' # AsciiDoc
-gem "json", "2.0.2"
+gem 'awestruct', '0.6.0.alpha1'
+gem 'asciidoctor', '1.5.6.1' # AsciiDoc
+gem 'json', '2.1.0'
--- _ext/haml/filters/asciidoc.rb
+++ _ext/haml/filters/asciidoc.rb
@@ -4,3 +4,4 @@ Haml::Filters.register_tilt_filter 'AsciiDoc'
 Haml::Filters::AsciiDoc.options[:safe] = :safe
 Haml::Filters::AsciiDoc.options[:attributes] ||= {}
 Haml::Filters::AsciiDoc.options[:attributes]['showtitle'] = ''
+Haml::Filters::AsciiDoc.template_class = Tilt::AsciidoctorTemplate
