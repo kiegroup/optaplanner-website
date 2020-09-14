e6a2d0b71b8806347d7a4d85ffdffd26ca6aa718
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2020-09-14 18:11:47 +0200

    Fix atomizer

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -10,6 +10,6 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 10)
   extension Awestruct::Extensions::Tagger.new(:posts, '/blog/index', '/blog/tags', :per_page => 10)
   # extension Awestruct::Extensions::Disqus.new
-  extension Awestruct::Extensions::Atomizer.new(:posts, '/blog/news.atom', :template => File.join( File.dirname(__FILE__), 'relative_template.atom.haml'))
+  extension Awestruct::Extensions::Atomizer.new(:posts, '/blog/news.atom', :template => '_ext/relative_template.atom.haml')
   extension Awestruct::Extensions::Sitemap.new()
 end
