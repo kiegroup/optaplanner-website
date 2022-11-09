966efc8998f900830c40c2804daca86256d00cca
radtriste <tradisso@redhat.com>
2022-11-09 10:58:02 +0100

    [PLANNER-2855] Migrate optaplanner publish job

---
+++ .ci/jenkins/dsl/jobs.groovy
@@ -0,0 +1,37 @@
+/*
+* This file is describing all the Jenkins jobs in the DSL format (see https://plugins.jenkins.io/job-dsl/)
+* needed by the Kogito pipelines.
+*
+* The main part of Jenkins job generation is defined into the https://github.com/kiegroup/kogito-pipelines repository.
+*
+* This file is making use of shared libraries defined in
+* https://github.com/kiegroup/kogito-pipelines/tree/main/dsl/seed/src/main/groovy/org/kie/jenkins/jobdsl.
+*/
+
+import org.kie.jenkins.jobdsl.model.Folder
+import org.kie.jenkins.jobdsl.KogitoJobTemplate
+import org.kie.jenkins.jobdsl.KogitoJobUtils
+import org.kie.jenkins.jobdsl.Utils
+
+jenkins_path = '.ci/jenkins'
+
+if (Utils.isMainBranch(this)){
+    setupWebsitePublishOtherJob()
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+void setupWebsitePublishOtherJob() {
+    def jobParams = KogitoJobUtils.getBasicJobParams(this, "optaplanner-website-publish", Folder.OTHER, "${jenkins_path}/Jenkinsfile.publish",
+            "This is a pipeline job for publishing automatically the Optaplanner Website.")
+    KogitoJobUtils.setupJobParamsDefaultMavenConfiguration(this, jobParams)
+    jobParams.triggers = [ push: true ]
+    jobParams.env.putAll([
+            JENKINS_EMAIL_CREDS_ID: "${JENKINS_EMAIL_CREDS_ID}",
+
+            BUILD_BRANCH_NAME: Utils.getGitBranch(this),
+            GIT_AUTHOR: Utils.getGitAuthor(this),
+            GIT_AUTHOR_CREDENTIALS_ID: Utils.getGitAuthorCredsId(this),
+    ])
+    KogitoJobTemplate.createPipelineJob(this, jobParams)
+}
